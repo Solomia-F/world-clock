0eaa0fdc30b4c0b3c9ee735031fe1a485b5561b6
Solomia-F <solomiia.fedyshyn@gmail.com>
2024-11-05 23:16:35 +0000

    Added more cities to the list

--- javascript/index.js
+++ javascript/index.js
@@ -5,11 +5,33 @@ function updateTime() {
     let currentCityDateElement = currentCityElement.querySelector(".date");
     let currentCityTimeElement = currentCityElement.querySelector(".time");
 
-    let cityTimeZone = moment.tz.guess().replace("_", " ").split("/")[1];
-    currentCityTimeZone.innerHTML = cityTimeZone;
-
+    currentCityTimeZone.innerHTML = moment.tz
+      .guess()
+      .replace("_", " ")
+      .split("/")[1];
     currentCityDateElement.innerHTML = moment().format("MMMM Do YYYY");
-    currentCityTimeElement.innerHTML = moment().format("h:mm:ss");
+    currentCityTimeElement.innerHTML = moment().format("HH:mm:ss");
+  }
+
+  let tokyoElement = document.querySelector("#tokyo");
+  if (tokyoElement) {
+    let tokyoDateElement = tokyoElement.querySelector(".date");
+    let tokyoTimeElement = tokyoElement.querySelector(".time");
+    let tokyoTime = moment().tz("Asia/Tokyo");
+
+    tokyoDateElement.innerHTML = tokyoTime.format("MMMM	Do YYYY");
+    tokyoTimeElement.innerHTML = tokyoTime.format("HH:mm:ss");
+  }
+
+  // Kyiv
+  let kyivElement = document.querySelector("#kyiv");
+  if (kyivElement) {
+    let kyivDateElement = kyivElement.querySelector(".date");
+    let kyivTimeElement = kyivElement.querySelector(".time");
+    let kyivTime = moment().tz("Europe/Kyiv");
+
+    kyivDateElement.innerHTML = kyivTime.format("MMMM	Do YYYY");
+    kyivTimeElement.innerHTML = kyivTime.format("HH:mm:ss");
   }
 }
 
@@ -18,15 +40,16 @@ function updateCity(event) {
     let cityTimeZone = event.target.value;
     let cityName = cityTimeZone.replace("_", " ").split("/")[1];
     let cityTime = moment().tz(cityTimeZone);
-    let citiesElement = document.querySelector("#selected-city");
+    let citiesElement = document.querySelector("#cities");
     citiesElement.innerHTML = ` 
-  
+   <div class="city">
         <div>
           <h2 id="city-title">${cityName}</h2>
           <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
         </div>
-        <div class="time">${cityTime.format("h:mm:ss")}</div>
+        <div class="time">${cityTime.format("HH:mm:ss")}</div>
       </div>
+    </div>
     `;
   }, 1000);
 }
